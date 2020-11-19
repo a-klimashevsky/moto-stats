a13d4d5f6caec385c43f9663c73358b6079b5e9c
Aliaksandr Klimasheuski <gladiator1988@gmail.com>
2020-11-19 21:43:11 +0300

    implemented dataclass

--- Offer.py
+++ Offer.py
@@ -1,11 +1,9 @@
-class Offer:
-    def __init__(self, id, mark, model, price, year):
-        self.id = id
-        self.mark = mark
-        self.model = model
-        self.price = price
-        self.year = year
+from dataclasses import dataclass
 
-    def __repr__(self):
-        return "Offer(id: %s, mark:%s, model:%s, year:%s, price: %s)" % (
-            self.id, self.mark, self.model, self.year, self.price)
+@dataclass
+class Offer:
+    id: str
+    mark: str
+    model: str
+    price: float
+    year: int
