cbcfb56898757ea0a7b8206affd8f53a082c156c
Aliaksandr Klimasheuski <gladiator1988@gmail.com>
2020-11-19 21:10:53 +0300

    used bokeh to display stats

--- analize_price.py
+++ analize_price.py
@@ -1,25 +1,43 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from bokeh.layouts import gridplot
+from bokeh.plotting import figure, output_file, show , ColumnDataSource
+from bokeh.models.ranges import DataRange1d
 import os
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
 path = os.path.join(__location__, "auto_ru_yamaha.csv")
 
 
 titanic = pd.read_csv(path)
 
-#print(titanic)
 
 glides = titanic[lambda x: x["model"] == "YZF_R6"]
 
-print(glides.groupby(["year"]).count()["price"])
-print(glides.groupby(["year"]).mean())
-ax1 = glides.groupby(["year"]).mean().plot()
+means = glides.groupby(["year"]).mean()
+diff = means.pct_change() * 100
+count = glides.groupby(["year"]).count()
 
-#ax2 = glides.groupby(["year"]).count()["price"].plot.bar(rot=0)
+print(count)
+p1 = figure(title="Yamaha YZF-R6", plot_height=350)
+p1.grid.grid_line_alpha=0.3
+p1.xaxis.axis_label = 'Year'
+p1.yaxis.axis_label = 'Price'
+p1.x_range = DataRange1d(start = 2021, end = 1995)
+p1.line(source = ColumnDataSource(means), x='year', y='price', color='#A6CEE3')
 
-#plt.xlim(1990, 2025)
-plt.title("Yamaha YZF-R6")
+p2 = figure(title = "Yamaha YZF-R6", plot_height=150)
+p2.xaxis.axis_label = 'Year'
+p2.yaxis.axis_label = 'Count'
+p2.vbar(source = ColumnDataSource(count),x = 'year', top='price', width=0.9)
+p2.x_range = DataRange1d(start = 2021, end = 1995)
 
-plt.show()
+p3 = figure(title = "Yamaha YZF-R6", plot_height=150)
+p3.xaxis.axis_label = 'Year'
+p3.yaxis.axis_label = 'Loose'
+p3.vbar(source = ColumnDataSource(diff),x = 'year', top='price', width=0.9)
+p3.x_range = DataRange1d(start = 2021, end = 1995)
+
+show(gridplot([[p1,None],[p2, None],[p3,None]]))
